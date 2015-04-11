f475aab2203ecbb3b5ab477d650d377e4f8015e2
Anton Dosov <dosantinc@gmail.com>
2015-04-11 15:57:27 +0300

    great expectations

--- FashionNewsFeed/FashionNewsFeedTests/FCHTTPClientTests.m
+++ FashionNewsFeed/FashionNewsFeedTests/FCHTTPClientTests.m
@@ -9,9 +9,13 @@
 
 @interface FCHTTPClientTests : XCTestCase
 
+
+
 @end
 
-@implementation FCHTTPClientTests : XCTestCase
+@implementation FCHTTPClientTests : XCTestCase{
+    NSObject* obj;
+}
 
 - (FCHTTPClient *)getsharedClient
 {
@@ -23,22 +27,58 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
+
+
 - (void)tearDown {
+    NSLog(@"hey");
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testGetPostWithId
 {
+    
+    XCTestExpectation *expectation =
+    [self expectationWithDescription:@"High Expectations"];
+    
     FCHTTPClient *client = [FCHTTPClient sharedClient];
     
     [client getPostById:1000
                 success:^(NSURLSessionDataTask *task, id responseObject){
-                    XCTAssert(YES, @"Pass");
+                    
+                    NSLog(@"The web page is %@ bytes long.", [responseObject description]);
+                    XCTAssert([responseObject description] > 0);
+                    [expectation fulfill];
+                    
+                    
+                    
+                    
+                    
+                    obj = responseObject;
+                    ([responseObject description]);
+                    [self debug:responseObject];
+                    NSString* str = (NSString* )[responseObject description];
+                    XCTAssert(YES, @"%@",str);
+                    
+                    
                 }
                 failure:^(NSURLSessionDataTask *task, NSError *error){
-                    XCTAssert(NO, @"Failed");
+                    XCTAssertFalse(@"Failed");
+                    
                 }];
+    
+    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *error) {
+        if (error) {
+            NSLog(@"Timeout Error: %@", error);
+        }
+    }];
+}
+
+
+
+
+-(void)debug: (id)response{
+    NSLog([response description]);
 }
 
 - (void)testPerformanceExample {
