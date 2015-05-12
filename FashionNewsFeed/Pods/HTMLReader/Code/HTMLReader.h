306a853762b31f6214b704e554156dd585919810
Anton Dosov <dosantinc@gmail.com>
2015-05-12 22:24:57 +0300

    100% pods

---
+++ FashionNewsFeed/Pods/HTMLReader/Code/HTMLReader.h
@@ -0,0 +1,10 @@
+//  HTMLReader.h
+//
+//  Public domain. https://github.com/nolanw/HTMLReader
+
+#import "HTMLDocument.h"
+#import "HTMLSelector.h"
+#import "HTMLSerialization.h"
+#import "NSString+HTMLEntities.h"
+#import "HTMLTreeEnumerator.h"
+#import "HTMLTextNode.h"
---
+++ FashionNewsFeed/Pods/Target Support Files/Pods-FashionNewsFeed-DateTools/Pods-FashionNewsFeed-DateTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FashionNewsFeed_DateTools : NSObject
+@end
+@implementation PodsDummy_Pods_FashionNewsFeed_DateTools
+@end
