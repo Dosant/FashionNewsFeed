90d4b0043b5a929be616898cdef65384714f8a79
Anton Dosov <dosantinc@gmail.com>
2015-07-09 18:00:27 +0300

    ReadMe

--- FashionNewsFeed/Pods/Target Support Files/Pods-FashionNewsFeed-GTScrollNavigationBar/Pods-FashionNewsFeed-GTScrollNavigationBar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FashionNewsFeed_GTScrollNavigationBar : NSObject
-@end
-@implementation PodsDummy_Pods_FashionNewsFeed_GTScrollNavigationBar
-@end
--- FashionNewsFeed/Pods/Target Support Files/Pods-FashionNewsFeedTests-AFNetworking/Pods-FashionNewsFeedTests-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FashionNewsFeedTests_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_FashionNewsFeedTests_AFNetworking
-@end
